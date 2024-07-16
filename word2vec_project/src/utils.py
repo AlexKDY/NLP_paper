ef4d01202ad36a9a3c948be8053d55e8d725ef25
Alex Kim <rlaehdbs5592@gmail.com>
2024-07-16 17:34:21 +0900

    implement word2vec paper
    implement utils.py, completed

--- word2vec_project/src/utils.py
+++ word2vec_project/src/utils.py
@@ -11,7 +11,7 @@ class DataReader:
 
     def __init__(self, inputFile, min_cnt):
 
-        self.negatives = dict()
+        self.negatives = []
         self.discards = dict()
         self.negpos = 0
 
@@ -19,6 +19,7 @@ class DataReader:
         self.id2word = dict()
         self.word_freq = dict()
         self.token_cnt = 0
+        self.sentece_cnt = 0
 
         self.inputFile = inputFile
         self.preprocess(min_cnt)
@@ -29,17 +30,15 @@ class DataReader:
         
         word_frequency = dict()
         with open(self.inputFile) as f:
-            line = f.readline
-            line = line.split()
-            if len(line) > 1:
-                self.sentences_count += 1
-                for word in line:
-                    if len(word) > 0:
-                        self.token_cnt += 1
-                        word_frequency[word] = word_frequency.get(word, 0) + 1
-
-
-
+            lines = f.readlines()
+            for line in lines:
+                line = line.split()
+                if len(line) > 1:
+                    self.sentence_cnt += 1
+                    for word in line:
+                        if len(word) > 0:
+                            self.token_cnt += 1
+                            word_frequency[word] = word_frequency.get(word, 0) + 1
 
         wid = 0
         for w, c in word_frequency.items():
@@ -53,25 +52,67 @@ class DataReader:
 
     def subSampling(self):
         t = 1e-5
-        self.discards = {word: 1 - np.sqrt(t / freq / self.token_cnt) for word, freq in self.word_freq.items}
+        f = np.array(list(self.word_frequency.values())) / self.token_cnt
+        self.discards = 1 - np.sqrt(t/f)
 
 
     def negativeSampling(self):
         freq_list = np.array(list(np.array(self.word_freq).values())) ** 0.75
         ratio_list = freq_list / np.sum(freq_list)
-        cnt_list = ratio_list *  DataReader.NEGATIVE_TABLE_SIZE
-        
-            
+        cnt_list = np.round(ratio_list *  DataReader.NEGATIVE_TABLE_SIZE)
+        for wid, c in enumerate(cnt_list):
+            self.negatives =[wid] * int(c)
+        self.negatives = np.array(self.negatives)
+        np.random.shuffle(self.negatives)
 
+    def getNegativeSample(self, target, size):
+        neg_sample = self.negatives[self.negpos:self.negpos + size]
+        self.negpos = (self.negpos + size) % len(self.negatives)
+        neg_sample = [neg for neg in neg_sample if neg not in target]
+        while len(neg_sample) < size:
+            additional = self.negatives[self.negpos: self.negpos + (size - len(neg_sample))]
+            self.negpos = (self.negpos + len(additional)) % len(self.negatives)
+            additional = [neg for neg in additional if neg not in target]
+            neg_sample.extend(additional)
+
+        return neg_sample
+
+        
 class skipGramDataset(Dataset):
     def __init__(self, data, context_size):
-        return
+        self.data = data
+        self.context_size = context_size
+        self.input_file = open(data.inputFile)
     
     def __len__(self):
-        return
+        return self.data.sentence_cnt
     
     def __getitem__(self, index):
-        return super().__getitem__(index)
+        while True:
+            line = self.input_file.readline()
+            if not line:
+                self.input_file.seek(0, 0)
+                line = self.input_file.readline()
+
+            if len(line) > 1:
+                words = line.split()
+
+                if len(words) > 1:
+                    sampled_ids = [self.data.word2id[word] for word in words if word in self.data.word2id.keys() and np.random.rand() > self.data.discards[self.word2id[word]]]
+                
+                    range = np.random.randint(1, self.context_size)                        
+                    
+                    return [(center, context, self.data.getNegativeSample([center, context], 5)) for c_id, center in enumerate(sampled_ids)
+                            for context in sampled_ids[max(0, c_id - range): min(c_id + range, len(sampled_ids))] if center != context]
+                
+    @staticmethod
+    def collate(batches):
+        all_u = [u for batch in batches for u, _, _ in batch if len(batch) > 0]
+        all_v = [v for batch in batches for _, v, _ in batch if len(batch) > 0]
+        all_neg_v = [neg_v for batch in batches for _, _, neg_v in batch if len(batch) > 0]
+
+        return torch.LongTensor(all_u), torch.LongTensor(all_v), torch.LongTensor(all_neg_v)
+
 
 
 
